684c542af9364d9a8b560300795ba828778a9bc6
clemilton <clemilton.fucapi@gmail.com>
2021-11-29 17:21:31 -0400

    projeto final

--- js/resultado.js
+++ js/resultado.js
@@ -1,37 +1,39 @@
-import {app,database} from './config.js'
-import {RTDatabase} from './classes/RTDatabase.js'
-import {Storage} from './classes/Storage.js'
+import { app, database } from './config.js'
+import { RTDatabase } from './classes/RTDatabase.js'
+import { Storage } from './classes/Storage.js'
 
 var CANDIDATOS;
 
 var tbody = document.getElementById('tbody-candidatos');
 
 
-RTDatabase.loadValueEvent('candidatos', (data) =>{
+RTDatabase.loadValueEvent('candidatos', (data) => {
 
-  tbody.innerHTML="";
+    tbody.innerHTML = "";
 
-  let num=1;
-  for(let index in data){
-    if(index!=-1 && index!=0){ // excluir votos brancos e nulo
-    
-      let candidato = data[index]; 
+    let num = 1;
+    for (let index in data) {
+        //if (index != -1 && index != 0) { // excluir votos brancos e nulo
 
-      let tr = document.createElement("tr");
+        let candidato = data[index];
 
-      let td1 = document.createElement("td");
-      td1.innerHTML= num;
-      tr.appendChild(td1);
-      let props = ["nome","numero","qtdVotos"];
-      props.forEach( (prop) => {
-        let td = document.createElement("td");
-        td.innerHTML = candidato[prop];
-        tr.appendChild(td);
-      });
+        let tr = document.createElement("tr");
 
-      tbody.appendChild(tr);
-      num = num+1;
+        let td1 = document.createElement("td");
+        td1.innerHTML = num;
+        tr.appendChild(td1);
+        let props = ["nome", "numero", "qtdVotos"];
+        props.forEach((prop) => {
+            let td = document.createElement("td");
+            td.innerHTML = candidato[prop];
+            tr.appendChild(td);
+        });
+
+        tbody.appendChild(tr);
+        num = num + 1;
+        // }
     }
-  }
+
+
 
 })
---
+++ js/urna2.js
@@ -0,0 +1,108 @@
+import { app, database } from './config.js'
+import { RTDatabase } from './classes/RTDatabase.js'
+import { Storage } from './classes/Storage.js'
+
+const CODIGO_URNA = 2;
+
+var divWaiting = document.getElementById("waiting");
+var divVotacao = document.getElementById("votacao");
+var inputVoto = document.getElementById("campo");
+var textNomeVoto = document.getElementById("textNomeVoto");
+var textNomeEleitor = document.getElementById("nomeEleitor")
+
+var confirmaVoto = document.getElementById("confirmaVoto");
+
+var progressbar = document.getElementById("progressbar");
+var KEY_VOTO;
+inputVoto.onchange = () => {
+    console.log(inputVoto.value)
+    let strVoto = inputVoto.value;
+    //SEM VOTO
+    if (strVoto == "") {
+        textNomeVoto.innerHTML = "Seu voto: "
+        KEY_VOTO = "";
+        return
+    } else if (strVoto == "0") { // VOTO BRANCO
+        textNomeVoto.innerHTML = "Seu voto: BRANCO"
+        KEY_VOTO = "0"
+        return;
+    }
+
+    if (!CANDIDATOS[strVoto]) {
+        // voto nulo
+        textNomeVoto.innerHTML = "Seu voto: NULO"
+        KEY_VOTO = "-1"
+        return;
+    } else {
+        textNomeVoto.innerHTML = "Seu voto: " + CANDIDATOS[strVoto].nome;
+        KEY_VOTO = CANDIDATOS[strVoto].numero
+    }
+
+}
+
+
+var INFO_URNA;
+var CANDIDATOS;
+
+RTDatabase.loadValueEvent('candidatos', (data) => {
+    CANDIDATOS = data;
+})
+
+
+function carregarUrna() {
+    RTDatabase.loadValueEvent('urnas/' + CODIGO_URNA, (data) => {
+
+
+        let urna = data;
+        let eleitor = data.eleitor;
+        if (urna.status == "DISPONIVEL") {
+            divWaiting.style.display = "block"
+            divVotacao.style.display = "none"
+
+        } else if (urna.status == "OCUPADA") {
+            divWaiting.style.display = "none"
+            divVotacao.style.display = "block"
+            inputVoto.value = ""
+            textNomeVoto.innerHTML = ""
+            textNomeEleitor.innerHTML = "Nome eleitor: " + eleitor.nome;
+            INFO_URNA = urna;
+        }
+    })
+}
+
+confirmaVoto.addEventListener("click", () => {
+    if (!KEY_VOTO) {
+        alert("insira um voto!");
+        return;
+    }
+
+    let candidato = CANDIDATOS[KEY_VOTO];
+
+    let updatesCandidato = {
+            qtdVotos: candidato.qtdVotos + 1
+        }
+        //atualizar a quantidade de votos +1 
+    RTDatabase.updateNode('candidatos/' + KEY_VOTO, updatesCandidato)
+        .then(() => {
+            let eleitor = INFO_URNA.eleitor;
+            let updatesEleitor = {
+                    status: "VOTOU"
+                }
+                //atualizar eleitor
+            RTDatabase.updateNode('eleitores/' + eleitor.key, updatesEleitor)
+                .then(() => {
+
+                    let updates = {
+                            status: "DISPONIVEL",
+                            eleitor: null
+                        }
+                        //atualizar urna
+                    RTDatabase.updateNode('urnas/' + INFO_URNA.id, updates);
+                })
+        })
+})
+
+
+
+
+carregarUrna();
